7f1df3d5ceed1a339272d3bcf8029e4a80a2278d
Antonio Wall <antoniowallferrazneto@yahoo.com.br>
2022-12-01 18:44:10 -0300

    Nova cena pro simcity

---
+++ Assets/Scripts/SimCity/Move.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField]private float speed;
+
+    private void Update()
+    {
+        PlayerMove();
+    }
+
+    private void PlayerMove()
+    {
+        Vector3 dir = Vector3.zero;
+        if(Input.GetKey(KeyCode.W))
+        {
+            dir = Vector3.forward;
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            dir = Vector3.left;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            dir = Vector3.right;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            dir = Vector3.back;
+        }
+
+        transform.position += dir * speed * Time.deltaTime;
+    }
+}
